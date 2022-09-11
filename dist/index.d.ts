9c4d1999bc64e202a47359c8db2e39d4f325087b
Andrei Muntean <andreymuntean2004@gmail.com>
2022-09-11 22:59:51 +0300

    Fix the declaration file

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,1 @@
-declare function decodeCNP(cnp: number): {
-  gender: "Male" | "Female",
-  birthDate: Date,
-  age: number,
-  county: string,
-  isResident: string
-}
+declare module 'cnp-decoder'
